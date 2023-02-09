e8b4b1030233a8f26c1fc29c571da598f37d5d2a
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 14:08:24 +0700

    TODO: tao ra branch Feature3 trong branch Develop

--- Player.cs
+++ Player.cs
@@ -7,6 +7,7 @@ public class Player
     private void Start() {
         count = 1;
         vector = new Vector3(0,0,0);
+        Debug.Log("TODO: tao ra branch Feature3 trong branch Develop");
     }
     
     private void OnCollisionEnter2D(Collision2D other) {
