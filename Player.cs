83e9e5d57c9a767447b697f605a03e7723533ed3
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 11:33:22 +0700

    add count value

--- Player.cs
+++ Player.cs
@@ -5,7 +5,7 @@ public class Player
     public Vector3 vector;
     public int count;
     private void Start() {
-                
+        count = 1;
     }
     
     private void OnCollisionEnter2D(Collision2D other) {
