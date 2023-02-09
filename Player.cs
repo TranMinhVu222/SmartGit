271e1a3983bcc1b78016e61a5225e218af953730
TranMinhVu222 <tranminhvu_t64@hus.edu.vn>
2023-02-09 11:19:39 +0700

    add Player script

---
+++ Player.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+public class Player
+{
+    public Transform a;
+    public Vector3 vector;
+    public int count;
+    private void Start() {
+        
+    }
+    private void Update() {
+        
+    }
+}
